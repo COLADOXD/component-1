6bdde563c49d4e184c8301d4fcfa1f3ca869e3a2
MateoChacon <xdmateo90@gmail.com>
2023-10-26 11:30:25 -0500

    50. v-for en un componente

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
 import ButtonCounter from "./component/ButtonCounter.vue"
 import BlogPost from "./component/BlogPost.vue";
+
+const posts = [
+  { title: "Post 01", id: 1, description: "Lorem ipsum dolor sit amet consectetur.", color: "primary" },
+  { title: "Post 02", id: 2, description: "Lorem ipsum dolor sit amet consectetur.", color: "secondary" },
+  { title: "Post 03", id: 3, color: "secondary" },
+]
 </script>
 
 <template>
@@ -9,8 +15,7 @@ import BlogPost from "./component/BlogPost.vue";
 
     <ButtonCounter />
     <button-counter />
-    <blog-post title="Post 01" :id="1" description="Lorem ipsum dolor sit amet consectetur." color="primary" />
-    <blog-post title="Post 02" :id="2" description="Lorem ipsum dolor sit amet consectetur." color="secondary" />
-    <blog-post title="Post 03" :id="3" color="success" />
+    <blog-post v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :color="post.color"
+      :description="post.description" />
   </div>
 </template>
