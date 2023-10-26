f3ebd6ddd599d104977b5c813743c296a46bfe64
MateoChacon <xdmateo90@gmail.com>
2023-10-26 11:05:01 -0500

    48. Múltiples props

--- src/component/BlogPost.vue
+++ src/component/BlogPost.vue
@@ -1,14 +1,14 @@
 <script setup>
 
-defineProps(['title'])
+defineProps(['title', 'id', 'description', 'color'])
 
 </script>
 
 <template>
     <div class="card mt-4">
         <div class="card-body">
-            <h5 class="card-tittle">{{ title }}</h5>
-            Lorem ipsum dolor sit amet.
+            <h5 class="card-tittle" :class="`text-${color}`">{{ id }}.{{ title }}</h5>
+            {{ description }}
         </div>
     </div>
 </template>
